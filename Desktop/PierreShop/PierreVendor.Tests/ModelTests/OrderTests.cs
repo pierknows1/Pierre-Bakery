0c1c3126984b23045ff449f7233519d5bd538437
pier <pierknows@gmail.com>
2023-07-24 07:46:48 -0400

    fix commit

---
+++ Desktop/PierreShop/PierreVendor.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierreVendor.Models;
+using System;
+
+namespace PierreVendor.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+
+  }
+}
