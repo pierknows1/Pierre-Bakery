1036f0a9316dbbf82823ccf2eb8411ca22521b99
pier <pierknows@gmail.com>
2023-07-24 11:30:59 -0400

    pass test for order description

--- Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/OrderTests.cs
+++ Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/OrderTests.cs
@@ -4,12 +4,20 @@ using PierreVendor.Models;
 namespace PierreVendor.Tests
 {
     [TestClass]
-    public class OrderTests{
+    public class OrderTests
+    {
         [TestMethod]
         public void OrderConstructor_CreatesInstanceOfOrder_Order()
         {
-            Order newOrder = new Order();
+            Order newOrder = new Order("Order Test", "Order Description");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+        [TestMethod]
+        public void OrderDescription_CheckForOrderDescription_string()
+        {
+            string orderDescription = "Order Description";
+            Order newOrder = new Order ("Order Test", orderDescription);
+            Assert.AreEqual(orderDescription, newOrder.Description);
         }
     }
 }
