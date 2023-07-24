2e0296083545c1797234054473109163b10332c0
pier <pierknows@gmail.com>
2023-07-24 09:51:29 -0400

    test pass for description and name

--- Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/VendorTests.cs
+++ Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/VendorTests.cs
@@ -1,5 +1,7 @@
+
 using System.Collections.Generic;
 using PierreVendor.Models;
+using System;
 
 namespace PierreVendor.Tests
 {
@@ -8,8 +10,26 @@ namespace PierreVendor.Tests
         [TestMethod]
         public void VendorConstructor_CreateInstanceOfVendor_Vendor()
         {
-            Vendor newVendor = new Vendor();
+            Vendor newVendor = new Vendor("name", "description");
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
         }
+    
+        [TestMethod]
+        public void Vendorname_ChecksName_Vendor()
+        {
+            string vendorName = "Vendor's name";
+            Vendor newVendor = new Vendor(vendorName, "Description");
+            Assert.AreEqual(vendorName, newVendor.Name);
+        }
+
+        [TestMethod]
+        public void VendorDescription_CreateDescription_Vendor()
+        {
+            string vendorDescription = "Description";
+            Vendor newVendor = new Vendor("Vendor's name", vendorDescription);
+            Assert.AreEqual(vendorDescription, newVendor.Description);
+
+        }
+
     }
 }
