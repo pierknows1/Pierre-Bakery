407fd967033929331522d540beff9bceb8778f55
pier <pierknows@gmail.com>
2023-07-24 10:51:06 -0400

    Pass test for order instance

--- Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/OrderTests.cs
+++ Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/OrderTests.cs
@@ -5,6 +5,11 @@ namespace PierreVendor.Tests
 {
     [TestClass]
     public class OrderTests{
-        
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order();
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
     }
 }
