fa1b9c427e27a46be1b8909fe69c1046ebaf02b5
pier <pierknows@gmail.com>
2023-07-24 10:08:30 -0400

    passing ID Test, getting correct id WHEN testing 1

--- Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/VendorTests.cs
+++ Desktop/Pierre-Vendor/PierreVendor.Tests/ModelTests/VendorTests.cs
@@ -6,7 +6,13 @@ using System;
 namespace PierreVendor.Tests
 {
      [TestClass]
-    public class VendorTests{
+    public class VendorTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Vendor.ClearAll();
+
+        }
         [TestMethod]
         public void VendorConstructor_CreateInstanceOfVendor_Vendor()
         {
@@ -28,6 +34,28 @@ namespace PierreVendor.Tests
             string vendorDescription = "Description";
             Vendor newVendor = new Vendor("Vendor's name", vendorDescription);
             Assert.AreEqual(vendorDescription, newVendor.Description);
+        }
+
+        [TestMethod]
+        public void GetVendorId_RetunId_Int()
+        {
+            Vendor newVendor = new Vendor ("vendorName", "Description");
+            int result = newVendor.Id;
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void GetAll_ReturnsAllVendors_List()
+        {
+            string vendor1 = "Vendor1";
+            string vendor2 = "Vendor2";
+            Vendor newVendor1 = new Vendor (vendor1, "description");
+            Vendor newVendor2 = new Vendor (vendor2, "description");
+            List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+            List<Vendor> result = Vendor.GetAll();
+            
+            CollectionAssert.AreEqual(newList, result);
+
 
         }
 
